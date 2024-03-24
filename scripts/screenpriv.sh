3d9e9baa6ab1760c24d5544831ad6dbd2bc18cef
nickx00 <nicholascaitong@gmail.com>
2024-03-24 18:46:54 -0300

    the last commit ...

--- scripts/screenpriv.sh
+++ scripts/screenpriv.sh
@@ -3,6 +3,8 @@ while true; do
 
 	if echo "$is_shared" | grep -qE '[0-9]+x[0-9]+.*xdg-desktop-portal-hyprland'; then
 		echo "true"
+	elif pgrep wf-recorder >/dev/null; then
+		echo "true"
 	else
 		echo "false"
 	fi
